db417ed600f3e07ebb2fc043aed6c3ff0c272611
gaishimo <gaithimo@gmail.com>
2022-09-08 16:21:01 +0900

    rLineTo example

--- SkiaComponent.tsx
+++ SkiaComponent.tsx
@@ -1,10 +1,14 @@
-import {Canvas, Circle} from '@shopify/react-native-skia';
+import {Canvas, Path, Skia} from '@shopify/react-native-skia';
 import React from 'react';
 
 export default function SkiaComponent() {
+  const path = Skia.Path.Make();
+  path.moveTo(100, 100);
+  path.lineTo(130, 130);
+  path.rLineTo(30, 30);
   return (
     <Canvas style={{flex: 1}}>
-      <Circle cx={100} cy={100} r={30} color="lightblue" />
+      <Path path={path} color="lightblue" style={'stroke'} strokeWidth={2} />
     </Canvas>
   );
 }
